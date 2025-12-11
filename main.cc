9f28f92c420b70ea2625bd45f9d9a16699b936a0
Carlo Mancini <carlo.mancini-terracciano@uniroma1.it>
2025-12-11 13:44:55 +0100

    using G4RunManagerFactory

--- main.cc
+++ main.cc
@@ -33,11 +33,7 @@
 
 #include "G4Types.hh"
 
-#ifdef G4MULTITHREADED
-#include "G4MTRunManager.hh"
-#else
-#include "G4RunManager.hh"
-#endif
+#include "G4RunManagerFactory.hh"
 
 #include "G4UImanager.hh"
 #include "Randomize.hh"
@@ -63,18 +59,9 @@ int main(int argc,char** argv) {
   //choose the Random engine
   G4Random::setTheEngine(new CLHEP::RanecuEngine);
 
-  // Construct the default run manager
-#ifdef G4MULTITHREADED
-  G4MTRunManager* runManager = new G4MTRunManager;
-  G4int nThreads = G4Threading::G4GetNumberOfCores();
-  if (argc==3) nThreads = G4UIcommand::ConvertToInt(argv[2]);
-  runManager->SetNumberOfThreads(nThreads);
-#else
-  //my Verbose output class
-  // G4VSteppingVerbose::SetInstance(new SteppingVerbose);
-  G4RunManager* runManager = new G4RunManager;
-#endif
-
+  // Construct the run manager
+  auto* runManager = G4RunManagerFactory::CreateRunManager(G4RunManagerType::Default);
+ 
   HistoManagerHelper::Instance();
   
   //set mandatory initialization classes
